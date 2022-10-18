2cb54e2da96a64b281dbffdedd4341b99d5c9ec3
DafneJ <85520965+DafneJ@users.noreply.github.com>
2022-10-18 18:16:17 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ self.addEventListener('install', (event) => {
             [
                 `${ROOT_PATH}/`, 
                 `${ROOT_PATH}/index.html`, 
-                  '/-pwa-gustavo/images/icons/android-launchericon-72-72.png',
+                  '/-AWP-U2-P7/images/images/icons/android-launchericon-72-72.png',
       '/-AWP-U2-P7/images/icons/android-launchericon-96-96.png',
       '/-AWP-U2-P7/images/icons/android-launchericon-144-144.png',
       '/-AWP-U2-P7/images/icons/android-launchericon-144-144.png',
